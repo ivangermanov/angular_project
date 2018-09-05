5b29456adf727f4d5e191a2c0f9635429873b5f5
Coman,Mara M.I <m.marairina@student.fontys.nl>
2018-09-05 14:11:21 +0200

    week 2 task done

---
+++ src/app/mock-departments.ts
@@ -0,0 +1,14 @@
+import { Department } from './department';
+
+export const DEPARTMENTS: Department[] = [
+  { id: 11, name: 'IT' },
+  { id: 12, name: 'Marketing' },
+  { id: 13, name: 'Accountancy' },
+  { id: 14, name: 'Media' },
+  { id: 15, name: 'Production' },
+  { id: 16, name: 'Service' },
+  { id: 17, name: 'Manufacturing' },
+  { id: 18, name: 'Secretary' },
+  { id: 19, name: 'Management' },
+ 
+];
