117b792e943d521742c16cebfcdb1010e5df885b
Germanov,Ivan I.P <i.germanov@student.fontys.nl>
2018-09-11 16:40:24 +0200

    Changed tasks in such a way that Task model also to which department a task belongs and which employees are working on it

--- src/app/task.ts
+++ src/app/task.ts
@@ -1,8 +1,13 @@
+import { Department } from './department';
+import { Employee } from './employee';
+
 export class Task {
     constructor(
         public id: number,
         public name: string,
         public reason: string,
-        public minutes: number
+        public minutes: number,
+        public employees: Employee[],
+        public department: Department
       ) { }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
-import { TASKS } from '../mock-tasks'
+import { Department } from '../department';
+import { Employee } from '../employee';
+
 import { TaskService } from '../task.service';
+//TODO: change after DEPARTMENTS gets a service
+import { DEPARTMENTS } from '../mock-departments';
+//TODO: change after EMPLOYEES gets a service
+import { EMPLOYEES } from '../mock-employees';
 
 @Component({
   selector: 'app-tasks',
@@ -11,18 +17,40 @@ import { TaskService } from '../task.service';
 export class TasksComponent implements OnInit {
 
   selectedTask: Task;
-
+  selectedEmployees: Employee[];
+  selectedDepartment: Department;
   tasks: Task[];
+
+  //TODO: change after DEPARTMENTS gets a service
+  departments = DEPARTMENTS;
+  //TODO: change after DEPARTMENTS gets a service
+  employees = EMPLOYEES;
 
   selectTask(task: Task) {
     this.selectedTask = task;
   }
 
-  add(name: string, reason: string, minutes: number): void {
+  selectEmployees(employees: Employee[]) {
+    this.selectedEmployees = employees;
+  }
+
+  selectDepartment(department: Department) {
+    this.selectedDepartment = department;
+  }
+
+  add(name: string, reason: string, minutes: number, employees: Employee[], department: Department): void {
+    console.log(this.selectedTask);
+    
     name = name.trim();
     if (!name) { return; }
-    const id = this.tasks[this.tasks.length - 1].id + 1;
-    this.taskService.addTask({id, name, reason, minutes} as Task).subscribe(task => { this.tasks.push(task); });
+    let id;
+    if (this.tasks.length != 0) {
+      id = this.tasks[this.tasks.length - 1].id + 1;
+    } else {
+      id = 1;
+    }
+    let newTask = new Task(id, name, reason, minutes, employees, department);
+    this.taskService.addTask(newTask).subscribe(task => { this.tasks.push(task); });
   }
 
   delete(task: Task): void {
@@ -34,7 +62,8 @@ export class TasksComponent implements OnInit {
     this.tasks = this.taskService.getTasks();
   }
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService) {
+  }
 
   ngOnInit() {
     this.getTasks();
