5a4167ce2b76d6463b913faa15dd60d98dddb3c0
Germanov,Ivan I.P <i.germanov@student.fontys.nl>
2018-10-14 23:50:10 +0200

    Added hash# routing for ease of use of Jaap :)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DepartmentsComponent } from './departments/departments.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { TaskDetailComponent }  from './task-detail/task-detail.component';
-import { EmployeeDetailComponent }  from './employee-detail/employee-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { DepartmentDetailComponent } from './departments-detail/departments-detail.component';
 
 const routes: Routes = [
   { path: 'departments', component: DepartmentsComponent },
-  { path: 'employees', component: EmployeesComponent},
-  { path: 'tasks', component: TasksComponent},
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'tasks', component: TasksComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/tasks/:id', component: TaskDetailComponent },
   { path: 'detail/employees/:id', component: EmployeeDetailComponent },
@@ -20,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
